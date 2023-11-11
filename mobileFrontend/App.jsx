c156ef2c23539cee95b04e2d6e7f6f7285f23053
zeeshanmnawaz786 <zeeshanm.nawaz786@gmail.com>
2023-11-11 06:56:18 -0800

    sdsf

--- mobileFrontend/App.jsx
+++ mobileFrontend/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
